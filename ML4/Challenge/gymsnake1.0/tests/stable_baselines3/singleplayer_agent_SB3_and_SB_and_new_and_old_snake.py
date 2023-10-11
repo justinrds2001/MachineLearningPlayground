3133e1a5a31669b1bd4e00cac91f96dbdcb75476
justinrodrigues@live.nl <you@example.com>
2023-10-11 20:20:48 +0200

    .

--- ML4/Challenge/gymsnake1.0/tests/stable_baselines3/singleplayer_agent_SB3_and_SB_and_new_and_old_snake.py
+++ ML4/Challenge/gymsnake1.0/tests/stable_baselines3/singleplayer_agent_SB3_and_SB_and_new_and_old_snake.py
@@ -1,18 +1,20 @@
 # Single-player agent using RL library Stable-Baselines3
 
 import logging
-import gym
-from gymsnake.envs.controller import Action  # performs the registration of snake-v1 with gym
+import gymnasium as gym
+# performs the registration of snake-v1 with gym
+from gymsnake.envs.controller import Action
 from stable_baselines3 import DQN, PPO
-#from stable_baselines import DQN, PPO2
+# from stable_baselines import DQN, PPO2
 
 log = logging.getLogger("snake_challenge")
 log.setLevel(logging.INFO)
-if not log.hasHandlers(): log.addHandler(logging.StreamHandler())
+if not log.hasHandlers():
+    log.addHandler(logging.StreamHandler())
 
 env = gym.make("snake-v1", disable_env_checker=True)
-#from gym_snake.envs.snake_env import SnakeEnv
-#env = SnakeEnv(snake_size=2, n_snakes=1, n_foods=1)
+# from gym_snake.envs.snake_env import SnakeEnv
+# env = SnakeEnv(snake_size=2, n_snakes=1, n_foods=1)
 
 obs = env.reset()
 env.render()
@@ -29,8 +31,9 @@ env = DummyVecEnv([lambda: SnakeEnv(n_snakes=2)])
 
 training = True
 if training:
-    #model = PPO2("MlpPolicy", env, verbose=1, tensorboard_log="tensorboard_logs/snake_dqn_agent/")
-    model = PPO("MlpPolicy", env, device="cpu", verbose=1, tensorboard_log="tensorboard_logs/snake_dqn_agent/")
+    # model = PPO2("MlpPolicy", env, verbose=1, tensorboard_log="tensorboard_logs/snake_dqn_agent/")
+    model = PPO("MlpPolicy", env, device="cpu", verbose=1,
+                tensorboard_log="tensorboard_logs/snake_dqn_agent/")
     # example how to tweak hyperparameters:
     # model = DQN("MlpPolicy", env, verbose=1, learning_rate=0.0005, gamma=0.99, policy_kwargs=dict(layers=[64, 64]),
     #             device="cpu", tensorboard_log="tensorboard_logs/snake_dqn_agent/")
@@ -39,7 +42,7 @@ if training:
     model.save("learned_models/snake_dqn_agent")
 else:
     model = PPO2.load("learned_models/snake_dqn_agent")
-    #TODO: model = PPO.load("learned_models/snake_dqn_agent")
+    # TODO: model = PPO.load("learned_models/snake_dqn_agent")
 
 log.info("finished training, now use the model to predict the optimal moves and render the env")
 obs = env.reset()
@@ -51,7 +54,7 @@ while not done:
     action, _states = model.predict(obs)
     log.info(f"predicted action: {action} for timestamp {timestamp}")
     obs, reward, done, info = env.step(action)
-    #TODO:obs, reward, done, info = env.step(Action(action))
+    # TODO:obs, reward, done, info = env.step(Action(action))
     log.info(f"reward: {reward}")
     env.render()
     timestamp += 1
